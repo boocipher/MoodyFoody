36dab4d4319864624189a25174fc2963310be4b8
Michael Klein <Michaelklein1997@gmail.com>
2022-01-13 12:56:41 -0600

    added elementary code for basic funcionality of saving items to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,11 +100,21 @@ var Bearer = 'Bearer ' + APIKEY   //needed for authentication header
 // }
 // }
 
+//LOCAL STORAGE COMPONENT
+var savedSearchResultsArray = JSON.parse(localStorage.getItem("savedSearches")) || []; //if savedSearch item exists in local storage, pull it.  Otherwise, initialize an empty array
+console.log(savedSearchResultsArray)
+// $('parentContainer').on('click', $('#saveButton'), function() {
+//   savedSearchResultsArray.push(searchResult);   //places searchResult object into savedSearchResultsArray
+//   localStorage.setItem("savedSearches", JSON.stringify(savedSearchResultsArray));  //saves updated savedSearchResultsArray to local storage
+// })
+
 
 //LOCAL STORAGE PSEUDOCODE
 //upon page load, favorites array will be parsed from local storage to initialize that variable (var savedSearchResultsArray)
-var savedSearchResultsArray = localStorage.parse()
+
 //user will click favorite button
+  //target save button
+  //Will need to use event delegation
   //pushes that restaurant search result object into var savedSearchResultsArray 
   //savedSearchResultsArray will be re-added to local storage
 
